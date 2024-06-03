ccb706a42262f469358309f805c68ef34512743c
fahadsultan0007 <fahadsultan000786@gmail.com>
2024-06-03 20:10:23 +0500

    firs test 2

--- test.py
+++ test.py
@@ -3,19 +3,22 @@ num_2=10
 def add_numbers(num_1 , num_2):
     result=num_1+num_2
     print(result)
-add_numbers(num_1 , num_2)
+
+    add_numbers(num_1 , num_2)
 
 
 def multiply_numbers(num_1 , num_2):
 #if type (num_1==int) and type (num_2==int)
     result=num_1*num_2
     print(result)
-multiply_numbers(num_1 , num_2)
+
+    multiply_numbers(num_1 , num_2)
 
 def subtract_numbers(num_1 , num_2):
  
     result=num_1-num_2
     print(result)
+
 subtract_numbers(num_1 , num_2)
 
 def divide_numbers(num_1 , num_2): 
@@ -24,6 +27,7 @@ def divide_numbers(num_1 , num_2):
         return
     result=num_1/num_2
     print(result)
+    
 divide_numbers(num_1 , num_2)
 
 
